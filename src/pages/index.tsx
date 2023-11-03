be799e94c28b4b4cbf1df5daa7fe3b398fc62c29
kjyook <106497893+kjyook@users.noreply.github.com>
2023-11-03 16:29:14 +0900

    check current user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
 import Navbar from "@/components/Navbar";
+import useCurrentUser from "@/hooks/userCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+const getServerSideProps = async (context: NextPageContext) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {}
+  }
+};
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
-      <Navbar />
-      
+      <h1 className="text-green-500">netflix clone</h1>
+      <p className="text-white">Logged in as : {user?.name}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>log out</button>
     </>
   )
 }
