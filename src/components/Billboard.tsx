fa0e4ecf61c0cb0598f295f121bb280dc61eb0b3
kjyook <106497893+kjyook@users.noreply.github.com>
2023-11-10 19:20:13 +0900

    docs : write readme

--- src/components/Billboard.tsx
+++ src/components/Billboard.tsx
@@ -1,5 +1,5 @@
 import useBillboard from "@/hooks/useBillboard";
-import React, { useEffect } from "react";
+import React from "react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 
 const Billboard = () => {
