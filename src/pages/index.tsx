9edbae5edd58737243ff3c7b71cb2f0bb9788d4f
kjyook <106497893+kjyook@users.noreply.github.com>
2023-11-13 23:28:46 +0900

    feat : MovieList component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,20 @@
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import useMovieList from "@/hooks/useMovieList";
 import useCurrentUser from "@/hooks/userCurrentUser";
 import { signOut } from "next-auth/react";
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   )
 }
